45edd767394f5593aab39b1cf9b90483a369b861
Greg Goz <gg@gg.pl>
2017-05-23 20:30:33 +0200

    Added butterknife fields in MainActivity

--- app/src/main/java/sdacademy/pl/simpleprojectmenager/MainActivity.java
+++ app/src/main/java/sdacademy/pl/simpleprojectmenager/MainActivity.java
@@ -1,16 +1,32 @@
 package sdacademy.pl.simpleprojectmenager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.login)
+    EditText login;
+    @BindView(R.id.password)
+    EditText password;
+    @BindView(R.id.sign)
+    Button sign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.sign)
+    public void onViewClicked() {
     }
 }
