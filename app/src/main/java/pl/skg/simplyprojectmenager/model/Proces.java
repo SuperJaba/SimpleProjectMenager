8e78af3164e9ea8f4aa9e38bd4b7e319754ffa03
Greg Goz <gg@gg.pl>
2017-06-01 20:54:57 +0200

    Added process create object in LoginActivity

--- app/src/main/java/pl/skg/simplyprojectmenager/model/Proces.java
+++ app/src/main/java/pl/skg/simplyprojectmenager/model/Proces.java
@@ -4,13 +4,25 @@ import java.io.Serializable;
 import java.util.List;
 
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class Proces implements Serializable {
-    private String process_ame;
+    private String process_name;
     private long proces_id;
     private String description;
     private int amount;
     private List<Step> steps;
 
+    public Proces() {
+    }
+
+    public Proces(String process_name, long proces_id, String description, int amount, List<Step> steps) {
+        this.process_name = process_name;
+        this.proces_id = proces_id;
+        this.description = description;
+        this.amount = amount;
+        this.steps = steps;
+    }
 }
--- app/src/main/java/pl/skg/simplyprojectmenager/model/Step.java
+++ app/src/main/java/pl/skg/simplyprojectmenager/model/Step.java
@@ -2,9 +2,24 @@ package pl.skg.simplyprojectmenager.model;
 
 import java.io.Serializable;
 
-class Step implements Serializable {
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Step implements Serializable {
     private String step_name;
     private int section_id;
     private boolean started;
     private boolean finished;
+
+    public Step() {
+    }
+
+    public Step(String step_name, int section_id, boolean started, boolean finished) {
+        this.step_name = step_name;
+        this.section_id = section_id;
+        this.started = started;
+        this.finished = finished;
+    }
 }
