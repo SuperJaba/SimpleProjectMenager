2a4f7a51e1fec6ac25489b62730206f2dda0908c
Marta <k.czechowski83@gmail.com>
2017-06-07 20:41:40 +0200

    actual version

--- app/src/main/java/pl/skg/simplyprojectmenager/model/Step.java
+++ app/src/main/java/pl/skg/simplyprojectmenager/model/Step.java
@@ -1,10 +1,8 @@
 package pl.skg.simplyprojectmenager.model;
 
 import java.io.Serializable;
-
 import lombok.Getter;
 import lombok.Setter;
-
 
 @Setter
 @Getter
@@ -14,47 +12,13 @@ public class Step implements Serializable {
     private int sectionId;
     private int status; //3 positions (0-not started, 1-doing, 2-finished)
 
-
     public Step() {
     }
 
-
     @Override
     public String toString() {
-        return stepName + ", " + sectionId + ", " + status + ", ";
+        return stepName +", "+ sectionId +", "+status;
     }
-
-//    public String getStepName() {
-//        return stepName;
-//    }
-//
-//    public void setStepName(String stepName) {
-//        this.stepName = stepName;
-//    }
-//
-//    public int getSectionId() {
-//        return sectionId;
-//    }
-//
-//    public void setSectionId(int sectionId) {
-//        this.sectionId = sectionId;
-//    }
-//
-//    public boolean isStarted() {
-//        return started;
-//    }
-//
-//    public void setStarted(boolean started) {
-//        this.started = started;
-//    }
-//
-//    public boolean isFinished() {
-//        return finished;
-//    }
-//
-//    public void setFinished(boolean finished) {
-//        this.finished = finished;
-//    }
 
     public Step(String stepName, int sectionId, int status) {
         this.stepName = stepName;
@@ -64,14 +28,7 @@ public class Step implements Serializable {
     }
 
 
-//    @Override
-//    public String toString() {
-//        return "Step{" +
-//                "stepName='" + stepName + '\'' +
-//                ", sectionId=" + sectionId +
-//                ", status=" + status +
-//                '}';
-//    }
+
 
     public String getStepName() {
         return stepName;
